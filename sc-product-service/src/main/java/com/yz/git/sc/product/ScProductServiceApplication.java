9607c71c888ea8aef384e855bc39e601a836fb0b
xuyang <xuyang7@le.com>
2019-08-21 09:56:10 +0800

    modify

--- sc-product-service/src/main/java/com/yz/git/sc/product/ScProductServiceApplication.java
+++ sc-product-service/src/main/java/com/yz/git/sc/product/ScProductServiceApplication.java
@@ -1,4 +1,4 @@
-package com.yz.git.sc.product.service;
+package com.yz.git.sc.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
