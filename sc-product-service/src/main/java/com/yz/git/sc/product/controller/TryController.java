fe398729ed219fd6e1f6cf9a16f7eee7c3403770
xuyang <xu789258123>
2019-12-11 15:57:44 +0800

    1、修改环境启动配置 2、Controller中修改

--- sc-product-service/src/main/java/com/yz/git/sc/product/controller/TryController.java
+++ sc-product-service/src/main/java/com/yz/git/sc/product/controller/TryController.java
@@ -1,8 +1,7 @@
 package com.yz.git.sc.product.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yz.git.sc.product.domain.ScProductInfo;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author xuyang
@@ -15,5 +14,24 @@ public class TryController {
     @GetMapping("/msg")
     public String tryMsg(){
         return "try Msg success083!";
+    }
+
+    @GetMapping("/pathv/{path}")
+    public String tryPath(@PathVariable("path")String path){
+        String path1 = path;
+        return path1;
+    }
+
+    @PostMapping("/request/{path}/{name}/{age}")
+    public String tryRequest(@RequestParam("path")String path,@RequestParam("name")String name,@RequestParam("path")Integer age){
+        String path1 = path;
+        String name1 = name;
+        Integer age1 = age;
+        return path1;
+    }
+    @PostMapping("/rb")
+    public ScProductInfo tryRequestBody(@RequestBody ScProductInfo sc){
+        ScProductInfo sc1 = sc;
+        return sc1;
     }
 }
